e3922574ebfe0b24d3d0d491dc92452404dea78f
Harshil-0707 <chaurasiyaharshil7705@gmail.com>
2024-04-12 00:44:51 +0530

    initial commit

---
+++ main.js
@@ -0,0 +1,8 @@
+const emoji = document.querySelectorAll(".emoji");
+const playerSign = document.querySelector(".playerSign");
+const computerScore = document.querySelector(".computerScore");
+emoji.forEach((emojis) => {
+  emojis.addEventListener("click", (e) => {
+    let index = e.currentTarget;
+  });
+});
